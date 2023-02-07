5b410ce90b30ef81f9810ae53bb4cf0f2a883388
Asif Sharif <asif-sharif@hotmail.com>
2023-02-07 21:16:36 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,13 @@ provider "aws" {
   access_key = "my-access-key"
   secret_key = "my-secret-key"
 }
+#main.tf
+resource "aws_iam_user" "example" {
+  count = "${length(var.username)}"
+  name = "${element(var.username,count.index )
+  path = "/system/"
+}
+resource "aws_iam_access_key" "newemp" {
+  count = length(var.username)
+  user = element(var.username,count.index)
+}
